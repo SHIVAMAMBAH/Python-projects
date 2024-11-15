e4f6e7a9c963c9d3dfca629278d5dae7f894d117
SDiHALF <shivamsharma22468@gmail.com>
2024-11-15 17:51:06 +0530

    Update prime_number_check.py

--- Basic Python/prime_number_check.py
+++ Basic Python/prime_number_check.py
@@ -1,1 +1,24 @@
+import math
 
+def is_prime(number:int)->str|bool:
+    """Checks if a number is prime."""
+    if number <= 1:
+        return False
+    if number in (2, 3):
+        return True
+    if number % 2 == 0:
+        return False
+    
+    sqrt_number = int(math.sqrt(number))
+    for i in range(3,sqrt_number+1,2):
+        if number%i==0:
+            return f"{number} is not prime"
+    
+    return f"{number} is prime"
+
+if __name__ == "__main__":
+    try:
+        number = int(input("Enter a number : "))
+        print(is_prime(number))
+    except ValueError:
+        print("Invalid Input")
