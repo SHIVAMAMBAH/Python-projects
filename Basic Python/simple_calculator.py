268d50a55d8438f90e68500cec7afca4dce03114
SDiHALF <shivamsharma22468@gmail.com>
2024-11-15 16:13:46 +0530

    Update simple_calculator.py

--- Basic Python/simple_calculator.py
+++ Basic Python/simple_calculator.py
@@ -1,1 +1,39 @@
+def simple_calculator(operand1:float,operator:str,operand2:float)->float:
+  """Function to perform basic arithmatic operations"""
+    match operator:
+        case '+':
+            return operand1+operand2
+        case '-':
+            return operand1-operand2
+        case '*':
+            return operand1*operand2
+        case '/':
+            if operand2 == 0:
+                raise ZeroDivisionError("Division by zero is not allowed.")
+            return operand1/operand2
+        case _:
+            raise ValueError(f"Unsupported operator : {operator}")
 
+
+result = 0
+
+if __name__ == "__main__":
+    try : 
+        while True : 
+            operand1 = float(input("Enter number : "))
+    
+            operator = input("Enter the operator(+,-,/,* or 'q' to quit) : ")
+            if(operator=="q"):
+                break
+    
+            operand2 = float(input("Enter number : "))
+
+            try:
+                result+= simple_calculator(operand1,operator,operand2)
+                print(result)
+            except (ZeroDivisionError,ValueError) as e:
+                print(f"Error : {e}")
+    except ValueError:
+        print("Invalid Input")
+    except KeyboardInterrupt:
+        print("\nCalculator terminates by user!")
