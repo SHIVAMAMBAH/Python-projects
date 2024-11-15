035e385259f40bd64fe15ac6583a3b664057a7e2
SDiHALF <shivamsharma22468@gmail.com>
2024-11-15 23:20:38 +0530

    Update perfect_numbers.py

--- Basic Python/perfect_numbers.py
+++ Basic Python/perfect_numbers.py
@@ -1,1 +1,18 @@
+def perfect_number(number:int)->str:
+    if number <= 1:
+        return f"{number} is not a perfect number."
+    
+    factors = [1]
+    for i in range(2,int(number**0.5)+1):
+        if number%i==0:
+            factors.append(i)
+            if i != number // i:
+                factors.append(number // i)
+    return f"{number} is {'' if number==sum(factors) else 'not'} perfect "
 
+if __name__ == "__main__":
+    try:
+        number = int(input("Enter the number : "))
+        print(perfect_number(number))
+    except ValueError:
+        print("Invalid Input")
