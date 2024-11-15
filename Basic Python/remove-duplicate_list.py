6120fff2187991a9427a58c8766ad4f38f8c586d
SDiHALF <shivamsharma22468@gmail.com>
2024-11-16 00:02:01 +0530

    Update remove-duplicate_list.py

--- Basic Python/remove-duplicate_list.py
+++ Basic Python/remove-duplicate_list.py
@@ -1,1 +1,19 @@
+def remove_duplicate_elements(lst:list)->list|str:
+    if not lst:
+        return f"{List} is empty"
+    if len(lst)==1:
+        return f"{List} has only one element"
+    
+    new_list = []
+    for element in lst:
+        if element not in new_list:
+            new_list.append(element)
+    
+    return new_list
 
+if __name__ == "__main__":
+    try:
+        List = [1,2,3,4,2,3,4]
+        print(remove_duplicate_elements(List))
+    except ValueError:
+        print("Invalid Input")
