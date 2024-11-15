185421f7b545e1cf6828a20b960b687a1ae1a19a
SDiHALF <shivamsharma22468@gmail.com>
2024-11-15 15:11:21 +0530

    Update check_odd_even.py

--- Basic Python/check_odd_even.py
+++ Basic Python/check_odd_even.py
@@ -1,1 +1,9 @@
+def check_odd_even(number: int)->str:
+    return f"{number} is {'odd' if number&1 else 'even'}"
 
+if __name__ == "__main__":
+    try:
+        number = int(input("Enter a number:"))
+        print(check_odd_even(number))
+    except ValueError:
+        print("Invalid Input. Pleae enter an integer.")
