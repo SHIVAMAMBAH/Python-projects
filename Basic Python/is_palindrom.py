9d44c667a896623d7a8c391ca45fcb25b5efa8e2
SDiHALF <shivamsharma22468@gmail.com>
2024-11-15 17:41:06 +0530

    Update is_palindrom.py

--- Basic Python/is_palindrom.py
+++ Basic Python/is_palindrom.py
@@ -1,1 +1,16 @@
+def is_palindrom(string:str)->str:
+    """Checks if the given string or numeric string is a palindrome."""
+    if string.isdigit():
+        return string == string[::-1]
+    
+    return string == string[::-1]
 
+if __name__ == "__main__":
+    try:
+        string = input("Enter a string :").strip()
+        if is_palindrom(string):
+            print(f"{string} is a palindrom")
+        else:
+            print(f"{string} is not a palindrom")
+    except ValueError:
+        print("Ivalid Input!")
