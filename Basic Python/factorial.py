3c808ae2e9aa5995be4aea8ce06f3eb18aedf991
SDiHALF <shivamsharma22468@gmail.com>
2024-11-15 16:41:46 +0530

    Update factorial.py

--- Basic Python/factorial.py
+++ Basic Python/factorial.py
@@ -1,4 +1,5 @@
 def factorial(number:int)->str|int:
+    """Calculates the factorial of non-negaive numbers."""
     if number<0:
         return "Factorial of negative numbers don't exist."
     if number in (0,1):
