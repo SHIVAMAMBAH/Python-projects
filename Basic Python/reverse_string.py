bc4d3b422da88f2d58859c39013e4674f0f57aed
SDiHALF <shivamsharma22468@gmail.com>
2024-11-16 12:55:26 +0530

    Update reverse_string.py

--- Basic Python/reverse_string.py
+++ Basic Python/reverse_string.py
@@ -3,6 +3,6 @@ def reverse_string(string:str)->str:
     return string[::-1]
 
 if __name__=="__main__":
-    string = input("Enter a string : ")
+    string:str = input("Enter a string : ")
     reversed_string = reverse_string(string)
     print(f"{string} -> {reversed_string}")
