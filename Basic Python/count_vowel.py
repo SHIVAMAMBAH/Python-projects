12ca19228ed698bd29f254242deddf2fa7d76d43
SDiHALF <shivamsharma22468@gmail.com>
2024-11-16 12:51:37 +0530

    Update count_vowel.py

--- Basic Python/count_vowel.py
+++ Basic Python/count_vowel.py
@@ -2,13 +2,13 @@ def vowel_count(string:str)->str:
     """To count the number of vowels in a string"""
     vowels = ['a','e','i','o','u']
     
-    count = sum(1 for char in string.lower() if char in vowels)
+    count:int = sum(1 for char in string.lower() if char in vowels)
     
     return f"{count} Vowels"
 
 if __name__ == "__main__":
     try:
-        string = input("Enter a string : ")
+        string:str = input("Enter a string : ")
         print(vowel_count(string)) 
     except ValueError:
         print("Invalid Input")   
