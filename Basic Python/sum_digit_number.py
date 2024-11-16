504af10743322d17da72562e83961af4899ed285
SDiHALF <shivamsharma22468@gmail.com>
2024-11-16 12:51:03 +0530

    Update sum_digit_number.py

--- Basic Python/sum_digit_number.py
+++ Basic Python/sum_digit_number.py
@@ -1,6 +1,6 @@
 def sum_of_digits_of_number(number:int)->int:
     """Function to  find the sum of digits of a number"""
-    result = 0
+    result:int = 0
     while number!=0:
         result+=number%10
         number//=10
@@ -8,7 +8,7 @@ def sum_of_digits_of_number(number:int)->int:
 
 if __name__ == "__main__":
     try:
-        number = int(input("Enter a number : "))
+        number:int = int(input("Enter a number : "))
         print(sum_of_digits_of_number(number))
     except ValueError:
         print("Invalid Input")
