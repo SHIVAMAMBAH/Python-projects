75e2ee108340558195c8046e5e45028db767cc95
SDiHALF <shivamsharma22468@gmail.com>
2024-11-16 12:52:27 +0530

    Update factorial.py

--- Basic Python/factorial.py
+++ Basic Python/factorial.py
@@ -4,14 +4,14 @@ def factorial(number:int)->str|int:
         return "Factorial of negative numbers don't exist."
     if number in (0,1):
         return "1"
-    result = 1
+    result:int = 1
     for i in range(2,number+1):
         result*=i
     return result
 
 if __name__ == "__main__":
     try:
-        number = int(input("Enter a number :"))
+        number:int = int(input("Enter a number :"))
         print(factorial(number))
     except ValueError:
         print("Invalid number input. Please Enter an Integer.")
